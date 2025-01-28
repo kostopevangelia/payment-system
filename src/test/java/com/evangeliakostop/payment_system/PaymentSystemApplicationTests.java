97f24bb4a22efcbcb82f8aaa9b4a4f94569f235a
evangeliakostop <evaggeliakostop@gmail.com>
2025-01-28 16:56:26 +0200

    initial project

---
+++ src/test/java/com/evangeliakostop/payment_system/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evangeliakostop.payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
