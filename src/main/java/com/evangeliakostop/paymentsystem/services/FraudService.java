c5b242786f347911be5362f26de257dfdec3fb7b
evangeliakostop <kostopevangelia@gmail.com>
2025-08-07 18:46:32 +0300

    initial set up for fraud api integration (not stable)

---
+++ src/main/java/com/evangeliakostop/paymentsystem/services/FraudService.java
@@ -0,0 +1,9 @@
+package com.evangeliakostop.paymentsystem.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class FraudService {
+}
--- src/main/java/com/evangeliakostop/paymentsystem/services/PaymentService.java
+++ src/main/java/com/evangeliakostop/paymentsystem/services/PaymentService.java
@@ -21,12 +21,14 @@ public class PaymentService {
     private final PaymentMsIntegration paymentMsIntegration;
     private final StripeIntegration stripe;
     private final PaymentsDBAccess paymentsDBAccess;
+    private final FraudService fraudService;
 
     @Autowired
-    public PaymentService(PaymentMsIntegration paymentMsIntegration, StripeIntegration stripe, PaymentsDBAccess paymentsDBAccess) {
+    public PaymentService(PaymentMsIntegration paymentMsIntegration, StripeIntegration stripe, PaymentsDBAccess paymentsDBAccess, FraudService fraudService) {
         this.paymentMsIntegration = paymentMsIntegration;
         this.stripe = stripe;
         this.paymentsDBAccess = paymentsDBAccess;
+        this.fraudService = fraudService;
     }
 
     /**
@@ -52,6 +54,8 @@ public class PaymentService {
                 paymentInfo = paymentMsIntegration.confirmPayment(request, paymentIntent, sessionId);
             }
             paymentsDBAccess.insertInitTransaction(transactionId, request.getTransactionType(), request.getAmount(), request.getCurrency());
+
+            fraudService.getfraudPrediction();
 
             return paymentInfo;
 
