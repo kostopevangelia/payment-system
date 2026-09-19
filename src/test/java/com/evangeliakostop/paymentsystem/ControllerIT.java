a113f46881fc3b68eda2f5c9269ffc65d810ca94
Evangelia Kostopoulou <kostopevangelia@gmail.com>
2026-09-20 00:20:09 +0300

    test: ControllerIT.java fix restTemplateStripe

--- src/test/java/com/evangeliakostop/paymentsystem/ControllerIT.java
+++ src/test/java/com/evangeliakostop/paymentsystem/ControllerIT.java
@@ -14,11 +14,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.client.RestTemplate;
@@ -27,7 +29,6 @@ import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -39,7 +40,8 @@ public class ControllerIT {
 
     @Mock
     private RestTemplate restTemplateFraudApi;
-    @Mock
+    @MockitoBean
+    @Qualifier("restTemplateStripe")
     private RestTemplate restTemplateStripe;
     @Autowired
     private MockMvc mockMvc;
